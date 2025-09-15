753d441a95d014a28410f89e01ce7768dc787233
S8287880177 <saurabhsj069@gmail.com>
2025-09-15 06:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+//print message in console
+console.log("Hello Acciojob");
+//change the text color dynamically(extra)
+document.querySelector(".hello-accio").style.color = "yellow";
